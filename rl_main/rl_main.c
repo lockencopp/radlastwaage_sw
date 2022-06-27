099ac3871026196a28b77a3bead7d3efcab62731
Christoph Deussen <christoph@deussen.one>
2022-06-27 20:11:41 +0000

    Implement SPI communication - Fix HX71708 readout

--- rl_main/rl_main.c
+++ rl_main/rl_main.c
@@ -42,19 +42,17 @@
 #define SPI_COM_RX 16
 #define SPI_COM_TX 19
 #define SPI_COM_SCK 18
-#define SPI_COM_CS0 18
-#define SPI_COM_CS1 18
-#define SPI_COM_CS2 18
-#define SPI_COM_CS3 18
+#define SPI_COM_CS0 25
+#define SPI_COM_CS1 24
+#define SPI_COM_CS2 23
+#define SPI_COM_CS3 22
 
 uint32_t timeNow = 0;
 uint32_t timeLast = 0;
 
-void printbuf(uint8_t buf[], size_t len)
-{
+void printbuf(uint8_t buf[], size_t len) {
     int i;
-    for (i = 0; i < len; ++i)
-    {
+    for (i = 0; i < len; ++i) {
         if (i % 16 == 15)
             printf("%02x\n", buf[i]);
         else
@@ -62,18 +60,17 @@ void printbuf(uint8_t buf[], size_t len)
     }
 
     // append trailing newline if there isn't one
-    if (i % 16)
-    {
+    if (i % 16) {
         putchar('\n');
     }
 }
 
-void init_hw()
-{
+void init_hw() {
     stdio_init_all();
 
     // Enable SPI 0 at 10kHz and connect to GPIOs
-    spi_init(spi_default, 10000);
+
+    spi_init(SPI_COM_PORT, 100 * 1000);
     spi_set_format(SPI_COM_PORT, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
     gpio_set_function(SPI_COM_RX, GPIO_FUNC_SPI);
     gpio_set_function(SPI_COM_SCK, GPIO_FUNC_SPI);
@@ -118,21 +115,27 @@ void init_hw()
     gpio_init(LED1);
     gpio_set_dir(LED1, GPIO_OUT);
 
-    gpio_init(LED1);
-    gpio_set_dir(LED1, GPIO_OUT);
+    gpio_init(LED2);
+    gpio_set_dir(LED2, GPIO_OUT);
 
-    gpio_init(LED1);
-    gpio_set_dir(LED1, GPIO_OUT);
+    gpio_init(LED3);
+    gpio_set_dir(LED3, GPIO_OUT);
 }
 
-int main()
-{
+char disp_buf[5];
+
+int main() {
     // Enable UART so we can print
     stdio_init_all();
 
     init_hw();
 
-    printf("SPI master example\n");
+    TFT_RedTab_Initialize();
+
+    setTextWrap(true);
+    TEST_DELAY1();
+    fillScreen(ST7735_BLACK);
+    setRotation(1);
 
     // gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
     // gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
@@ -144,42 +147,67 @@ int main()
     uint8_t out_buf[BUF_LEN], in_buf[BUF_LEN];
 
     // Initialize output buffer
-    for (size_t i = 0; i < BUF_LEN; ++i)
-    {
+    for (size_t i = 0; i < BUF_LEN; ++i) {
         out_buf[i] = i;
     }
 
-    printf("SPI master says: The following buffer will be written to MOSI endlessly:\n");
     printbuf(out_buf, BUF_LEN);
 
-    while (1)
-    {
-        timeNow = time_us_32();
+    int cnt = 0;
 
-        if (timeNow != timeLast)
-        {
-            if ((timeNow % 10000) == 0)
-            {
+    while (1) {
+        timeNow = time_us_32() / 1000;
+
+        if (timeNow != timeLast) {
+            if ((timeNow % 10) == 0) {
             }
-            if ((timeNow % 100000) == 0)
-            {
+            if ((timeNow % 100) == 0) {
             }
-            if ((timeNow % 1000000) == 0)
-            {
+            if ((timeNow % 500) == 0) {
                 // Write the output buffer to MOSI, and at the same time read from MISO.
-                gpio_put(SPI_COM_CS0,0);
-                
-                while(gpio_get(SPI_COM_RX) == 0)
-                {
-                    __asm__("NOP");
-                }
-                spi_write_read_blocking(spi_default, out_buf, in_buf, BUF_LEN);
-    
-                gpio_put(SPI_COM_CS0,1);
-                // Write to stdio whatever came in on the MISO line.
-                printbuf(in_buf, BUF_LEN);
 
-                // Sleep for ten seconds so you get a chance to read the output.
+                gpio_put(SPI_COM_CS0, 0);
+
+                sleep_ms(1);
+
+                spi_write_read_blocking(SPI_COM_PORT, out_buf, in_buf, BUF_LEN);
+
+                int result = (in_buf[0] << 24) | (in_buf[1] << 16) | (in_buf[2] << 8) | in_buf[3];
+
+                float result_f = result / 13200.0f;
+
+                sleep_ms(1);
+
+                gpio_put(SPI_COM_CS0, 1);
+                // Write to stdio whatever came in on the MISO line.
+                printf("OUT:\n");
+                printbuf(out_buf, BUF_LEN);
+                printf("IN:\n");
+                printbuf(in_buf, BUF_LEN);
+                printf("DISP:\n");
+                printf("%d\n", result);
+                sprintf(disp_buf, "%3.1f", result_f);
+
+                /*
+                printf("%d\n", cnt);
+
+                // fillScreen(ST7735_BLACK);
+                if (cnt < 10)
+                {
+                    sprintf(disp_buf, " %d", cnt);
+                }
+                else
+                {
+                    sprintf(disp_buf, "%d", cnt);
+                }
+                */
+                drawText(10, 10, disp_buf, ST7735_WHITE, ST7735_BLACK, 3);
+                /*
+                drawText(80, 10, disp_buf, ST7735_WHITE, ST7735_BLACK, 4);
+                cnt++;
+                if (cnt == 99)
+                    cnt = 0;
+                */
                 gpio_xor_mask(1 << LED0);
             }
         }
--- rl_sub/rl_sub.c
+++ rl_sub/rl_sub.c
@@ -31,9 +31,35 @@
 
 #define BUF_LEN         0x04
 
+#define HX1_DOUT 0
+#define HX1_SCK 1
+#define HX2_DOUT 2
+#define HX2_SCK 3
+
+#define SPI_COM_PORT spi0
+#define SPI_COM_RX 16
+#define SPI_COM_TX 19
+#define SPI_COM_SCK 18
+#define SPI_COM_CS 17
+
 int read_flag = 0;
 int timerval1 = 0;
 int timerval2 = 0;
+
+const uint LED_PIN = 25;
+uint32_t timeNow = 0;
+uint32_t timeLast = 0;
+int32_t hx1_data = 0;
+int32_t hx2_data = 0;
+uint32_t sampleNow = 0;
+uint32_t sampleLast = 0;
+uint32_t sampleNow2 = 0;
+uint32_t sampleLast2 = 0;
+uint32_t sampleTime = 0;
+uint32_t sampleTime2 = 0;
+int offset1 = 0;
+int offset2 = 0;
+int32_t offsetCnt = 0;
 
 void printbuf(uint8_t buf[], size_t len) {
     int i;
@@ -53,41 +79,84 @@ void printbuf(uint8_t buf[], size_t len) {
 void gpio_callback(uint gpio, uint32_t events) {
     // Put the GPIO event(s) that just happened into event_str
     // so we can print it
-    if(gpio_get(gpio)){
+    if (gpio_get(gpio)) {
         read_flag = 1;
         timerval2 = time_us_64();
-    }
-    else{
+        printf("1\n");
+    } else {
         timerval1 = time_us_64();
+        printf("2\n");
     }
+}
+
+inline void delay_asm(int clocks) {
+    for (int i = 0; i < clocks; i++) {
+        __asm__("NOP");
+    }
+}
+
+int HX711_init() {
+    gpio_init(HX1_DOUT);
+    gpio_init(HX1_SCK);
+    gpio_init(HX2_DOUT);
+    gpio_init(HX2_SCK);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_dir(HX1_DOUT, GPIO_IN);
+    gpio_set_dir(HX2_DOUT, GPIO_IN);
+    gpio_set_dir(HX1_SCK, GPIO_OUT);
+    gpio_set_dir(HX2_SCK, GPIO_OUT);
+
+    gpio_put(HX1_SCK, 0);
+    gpio_put(HX2_SCK, 0);
+    gpio_put(LED_PIN, 0);
+}
+
+int HX711_read(uint HX_DOUT, uint HX_SCK, int *offset) {
+    int hx_data = 0;
+    int j = 23;
+
+    for (int i = 0; i < 25; i++) {
+        gpio_put(HX_SCK, 1);
+
+        if (j >= 0)
+            hx_data |= (gpio_get(HX_DOUT) << j);
+        else
+            delay_asm(2);
+
+        j--;
+
+        gpio_put(HX_SCK, 0);
+        delay_asm(3);
+    }
+    if (hx_data > 0x7fffff) {
+        hx_data -= 0x1000000;
+    }
+    if ((offsetCnt <= 50)) {
+        return 0;
+    } else if (*offset == 0) {
+        *offset = hx_data;
+    }
+    return hx_data - *offset;
 }
 
 int main() {
     // Enable UART so we can print
     stdio_init_all();
-#if !defined(spi_default) || !defined(PICO_DEFAULT_SPI_SCK_PIN) || !defined(PICO_DEFAULT_SPI_TX_PIN) || !defined(PICO_DEFAULT_SPI_RX_PIN) || !defined(PICO_DEFAULT_SPI_CSN_PIN)
-#warning spi/spi_slave example requires a board with SPI pins
-    puts("Default SPI pins were not defined");
-#else
-
-    printf("SPI slave example\n");
-
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     // Enable SPI 0 at 1 MHz and connect to GPIOs
-    spi_init(spi_default, 1000 * 1000);
-    spi_set_format(spi_default, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
-    spi_set_slave(spi_default, true);
-    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI);
-    gpio_set_irq_enabled_with_callback(PICO_DEFAULT_SPI_CSN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
-    
+    spi_init(SPI_COM_PORT, 100 * 1000);
+    spi_set_format(SPI_COM_PORT, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
+    spi_set_slave(SPI_COM_PORT, true);
+    gpio_set_function(SPI_COM_RX, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_COM_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_COM_TX, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_COM_CS, GPIO_FUNC_SPI);
+    gpio_set_irq_enabled_with_callback(SPI_COM_CS, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
-    // Make the SPI pins available to picotool
-    bi_decl(bi_3pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI));
+    //gpio_init(SPI_COM_TX);
+    //gpio_set_dir(SPI_COM_TX,GPIO_OUT);
+    //gpio_put(SPI_COM_TX,1);
+    //gpio_pull_up(SPI_COM_TX);
 
     uint8_t out_buf[BUF_LEN], in_buf[BUF_LEN];
 
@@ -97,30 +166,73 @@ int main() {
         out_buf[i] = ~i;
     }
 
-    printf("SPI slave says: When reading from MOSI, the following buffer will be written to MISO:\n");
-    printbuf(out_buf, BUF_LEN);
-    
-    for (size_t i = 0; ; ++i) {
+    HX711_init();
+
+    printf("Start!\n");
+
+    hx1_data = HX711_read(HX1_DOUT, HX1_SCK, &offset1);
+    hx2_data = HX711_read(HX2_DOUT, HX2_SCK, &offset2);
+
+    while (1) {
         // Write the output buffer to MISO, and at the same time read from MOSI.
         //spi_write_read_blocking(spi_default, out_buf, in_buf, BUF_LEN);
-        
-        if(read_flag == 1)
-        {
 
-            // Write to stdio whatever came in on the MOSI line.
-            for (size_t i = 0; i < BUF_LEN; ++i) {
-                in_buf[i] = spi_get_hw(spi_default)->dr;
-            }
+        if (read_flag == 1) {
+            int32_t result = (hx1_data + hx2_data);
+            //int32_t result = time_us_32();
 
-            printf("SPI slave says: read page %d from the MOSI line:\n", i);
+            out_buf[0] = (uint8_t)((result >> 24) & 0xFF);
+            out_buf[1] = (uint8_t)((result >> 16) & 0xFF);
+            out_buf[2] = (uint8_t)((result >> 8) & 0xFF);
+            out_buf[3] = (uint8_t)(result & 0xFF);
+
+            //gpio_put(SPI_COM_TX,0);
+
+            //gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+
+            spi_write_read_blocking(SPI_COM_PORT, out_buf, in_buf, BUF_LEN);
+            /*
+            printf("OUT:\n");
+            printf("%3.1f\n", result/26400.0f);
+            printbuf(out_buf, BUF_LEN);
+            printf("IN:\n");
             printbuf(in_buf, BUF_LEN);
             printf("Timerdiff: %d", timerval2-timerval1);
+
+            */
             read_flag = 0;
+
+            //gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SIO);
+            //gpio_put(SPI_COM_TX,1);
         }
 
-        
-        gpio_xor_mask(1 << PICO_DEFAULT_LED_PIN);
-        sleep_ms(1000);
+        timeNow = time_us_32() / 1000;
+
+
+        if (timeNow != timeLast) {
+            if ((timeNow % 10) == 0) {
+                offsetCnt++;
+                if (!gpio_get(HX1_DOUT) && !gpio_get(HX2_DOUT)) {
+                    sampleNow=timeNow;
+                    sampleTime=(sampleNow - sampleLast) / 1000;
+                    sampleLast=sampleNow;
+                    hx1_data = HX711_read(HX1_DOUT, HX1_SCK, &offset1);
+                    hx2_data = HX711_read(HX2_DOUT, HX2_SCK, &offset2);
+                }
+            }
+            if ((timeNow % 100) == 0) {
+                int32_t result1 = hx1_data;
+                int32_t result2 = hx2_data;
+
+                printf("%c%c%c%c", 0x1B, 0x5B, 0x32, 0x4A);
+                printf("HX1: %.1f\n", (hx1_data / 13.2));
+                printf("HX2: %.1f\n", (hx2_data / 13.2));
+                printf("Total: %.1f\t%d\n", ((hx1_data + hx2_data) / 13.2), sampleTime);
+                printf("Timerdiff: %d\n", timerval2 - timerval1);
+
+                gpio_xor_mask(1 << LED_PIN);
+            }
+            timeLast = timeNow;
+        }
     }
-#endif
 }
